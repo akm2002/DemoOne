cd4ce4eb9ba0419bc969a50c2f0fb5fc2d9abed1
akm200 <km.lu@live.cn>
2018-01-18 18:20:39 +0800

    test

---
+++ src/main/java/com/pactera/utils/DemoCase.java
@@ -0,0 +1,17 @@
+package com.pactera.utils;
+
+public class DemoCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static String MSG = "Test";
+	
+	
+	public static void saySomething() {
+		
+		System.out.println(MSG);
+	}
+}
---
+++ src/test/java/com/pactera/utils/DemoCaseTest.java
@@ -0,0 +1,14 @@
+package com.pactera.utils;
+
+import org.junit.Test;
+
+public class DemoCaseTest {
+
+
+	
+	@Test
+	public static void testSaySomething() {
+		
+		DemoCase.saySomething();
+	}
+}
